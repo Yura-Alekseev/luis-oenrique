bb87494e8e3b15362de0564255c920dc9c92ee9d
yuraalekseev00@gmail.com <yuraalekseev00@gmail.com>
2019-04-20 19:28:58 +0300

    added my skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { KnowMoreComponent } from './know-more/know-more.component';
 import { ServicesComponent } from './services/services.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     KnowMoreComponent,
-    ServicesComponent
+    ServicesComponent,
+    MySkillsComponent
   ],
   imports: [
     BrowserModule
