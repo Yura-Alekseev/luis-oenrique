c09225208e1d5d1cb515e66bd5d34adaee280571
yuraalekseev00@gmail.com <yuraalekseev00@gmail.com>
2019-04-20 18:58:10 +0300

    added services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { KnowMoreComponent } from './know-more/know-more.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    KnowMoreComponent
+    KnowMoreComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
