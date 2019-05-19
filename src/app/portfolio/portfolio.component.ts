fb2f12e7e660a38fff6331760c00d15b1b4d33bb
yuraalekseev00@gmail.com <yuraalekseev00@gmail.com>
2019-05-19 17:55:08 +0300

    removed some photos

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -14,18 +14,6 @@ export class PortfolioComponent implements OnInit {
     {link: 'javascript:;', src: '../../assets/img/your-music.png', alt: 'your-music'},
     {link: 'javascript:;', src: '../../assets/img/build.png', alt: 'build'},
     {link: 'javascript:;', src: '../../assets/img/make.png', alt: 'make'},
-    {link: 'javascript:;', src: '../../assets/img/hello.png', alt: 'hello'},
-    {link: 'javascript:;', src: '../../assets/img/wanaka.png', alt: 'wanaka'},
-    {link: 'javascript:;', src: '../../assets/img/perfomance.png', alt: 'perfomance'},
-    {link: 'javascript:;', src: '../../assets/img/your-music.png', alt: 'your-music'},
-    {link: 'javascript:;', src: '../../assets/img/build.png', alt: 'build'},
-    {link: 'javascript:;', src: '../../assets/img/make.png', alt: 'make'},
-    {link: 'javascript:;', src: '../../assets/img/hello.png', alt: 'hello'},
-    {link: 'javascript:;', src: '../../assets/img/wanaka.png', alt: 'wanaka'},
-    {link: 'javascript:;', src: '../../assets/img/perfomance.png', alt: 'perfomance'},
-    {link: 'javascript:;', src: '../../assets/img/your-music.png', alt: 'your-music'},
-    {link: 'javascript:;', src: '../../assets/img/build.png', alt: 'build'},
-    {link: 'javascript:;', src: '../../assets/img/make.png', alt: 'make'},
   ];
 
   constructor() {
@@ -35,5 +23,6 @@ export class PortfolioComponent implements OnInit {
   }
 
   showMore() {
+
   }
 }
